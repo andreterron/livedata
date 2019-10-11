3373cf732b0e904b69c55fc35e854f4fc24ac21f
Andre Terron <git@terron.me>
2019-10-10 22:06:17 -0700

    Deleting old code for new branch

--- src/firestore/index.ts
+++
@@ -1,6 +0,0 @@
-// export { FirestoreOneToOneLiveList } from './firestore-one-to-one-live-list';
-// export { FirestoreManyToManyLiveList } from './firestore-many-to-many-live-list';
-// // export { FirestoreIdLiveObject } from './firestore-id-live-object';
-// export { FirestoreQueryLiveList } from './firestore-query-live-list';
-// export { FirestoreOneToManyLiveList } from './firestore-one-to-many-live-list';
-// export { FirestoreDataManager } from './firestore-data-manager';
--- src/jsdata/index.ts
+++
@@ -1,6 +0,0 @@
-export { JSDataOneToOneLiveList } from './jsdata-one-to-one-live-list';
-export { JSDataManyToManyLiveList } from './jsdata-many-to-many-live-list';
-export { JSDataIdLiveObject } from './jsdata-id-live-object';
-export { JSDataQueryLiveList } from './jsdata-query-live-list';
-export { JSDataOneToManyLiveList } from './jsdata-one-to-many-live-list';
-export { JSDataDataManager } from './jsdata-data-manager';
